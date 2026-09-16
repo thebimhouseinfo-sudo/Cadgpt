bbcb4b33682ffea20f756575c02644eab9357555
namtrinh <thebimhouse.info@gmail.com>
2026-09-16 11:35:24 +0700

    Migrate CAD MCP stderr logger

---
+++ runtimes/cad-mcp/utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+from config import DEBUG
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(
+            logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+        )
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+    return logger
